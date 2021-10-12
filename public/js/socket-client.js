cf48273973b2fc6fe4fc92af3d8c5f934575a315
DarkAstaroth <mninahuanca4@gmail.com>
2021-10-12 09:46:29 -0400

    Mensajes de conexion y desconexion

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,16 @@
+// referencias de html
+const lblOnline = document.querySelector("#lblOnline");
+const lblOffline = document.querySelector("#lblOffline");
+
 const socket = io();
+socket.on("connect", () => {
+  console.log("conectado");
+  lblOffline.style.display = "none";
+  lblOnline.style.display = "";
+});
+
+socket.on("disconnect", () => {
+  console.log("conectado");
+  lblOffline.style.display = "";
+  lblOnline.style.display = "none";
+});
