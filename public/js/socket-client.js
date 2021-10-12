253d99bf7b43ae3e9ff1b1811fccbe7860e4b2b6
DarkAstaroth <mninahuanca4@gmail.com>
2021-10-12 11:52:26 -0400

    Emitir desde cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,6 +1,8 @@
 // referencias de html
 const lblOnline = document.querySelector("#lblOnline");
 const lblOffline = document.querySelector("#lblOffline");
+const txtMensaje = document.querySelector("#txtMensaje");
+const btnEnviar = document.querySelector("#btnEnviar");
 
 const socket = io();
 socket.on("connect", () => {
@@ -13,4 +15,14 @@ socket.on("disconnect", () => {
   console.log("conectado");
   lblOffline.style.display = "";
   lblOnline.style.display = "none";
+});
+
+btnEnviar.addEventListener("click", () => {
+  const mensaje = txtMensaje.value;
+  const payload = {
+    mensaje,
+    id: "123ABC",
+    fecha: new Date().getTime(),
+  };
+  socket.emit("enviar-mensaje", payload);
 });
